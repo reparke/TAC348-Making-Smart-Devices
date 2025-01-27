05dd7c909d68ac396b42bdb6b3472e37b7ce1568
reparke <reparke@gmail.com>
2025-01-27 10:05:03 -0800

    updated w2

---
+++ _exercises/week02/leds_20251_mw/src/leds_20251_mw.cpp
@@ -0,0 +1,72 @@
+#include "Particle.h"
+
+SYSTEM_MODE(AUTOMATIC);
+
+SYSTEM_THREAD(ENABLED);
+
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+/*
+  New projects - every time
+  1) configure for device
+  2) create pin labels - GLOBAL CONSTANT that makes it easy to refer to our components
+  3) set the pin mode - we have to say if it is INPUT or OUTPUT
+
+  to use serial monitor
+  - serial.begin(9600) in setup
+  - serial.println(...)in loop
+*/
+//global constant - defined ABOVE setup
+const int PIN_LED = A2;
+const int PIN_POT = A0;
+
+//global var
+int brightness = 0;
+
+//setup runs once when we start the device
+void setup() {
+  //set pin mode
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_POT, INPUT);
+  Serial.begin(9600);   //config serial
+}
+
+
+
+//loop runs forever infinitely
+void loop() {
+  // //turn on LED for 2 sec
+  // // LED is a DIGITAL OUTPUT device
+  // //    ON or OFF
+  // digitalWrite(PIN_LED, HIGH);    //HIGH and LOW to be ON and OFF (or 1 and 0)
+  // delay(2000); //2000 millsec or 2 sec
+  // //turn off for 4 sec
+  // digitalWrite(PIN_LED, LOW);
+  // delay(4000);
+
+  //experiment with PWM (analog output)
+  // analogWrite(PIN_LED, 0);  //off
+  // delay(1000);
+  // analogWrite(PIN_LED, 127); //half brightness: 255 is full (3.3) so HALF would 127
+  // delay(1000);
+  // analogWrite(PIN_LED, 255);
+  // delay(1000);
+
+  //how can we increase the brightness AUTOMATICALLY
+  //increase brightness from 0 to 255, and reset back to 0
+  //we could use loop to increase brightness alittle each time
+  // void loop() is ALREADY an infinite loop
+
+  //remember that local var disappear each time the functions ends
+  //so if want to store brightness, it needs to GLOBAL
+
+  brightness = brightness + 10; //update
+  //reset
+  if (brightness >= 255) {
+    brightness = 0;
+  }
+  Serial.println("The brightness is " + String(brightness));
+  analogWrite(PIN_LED, brightness);
+  delay(500);
+ 
+}
---
+++ _exercises/week02/leds_20251_tth/src/leds_20251_tth.cpp
@@ -0,0 +1,73 @@
+#include "Particle.h"
+SYSTEM_MODE(AUTOMATIC);
+
+SYSTEM_THREAD(ENABLED);
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+/*
+  New projects
+  1) configure project for device
+  2) create pin labels for any pins we are using
+      --> create GLOBAL CONSTANTS to identify the pins we are using
+  3) set the pin mode --> in setup(), we say if a pin is INPUT or OUTPUT
+*/
+
+/*
+  global variables exist everywhere -- use these when we need a variable
+      to keep its value after the loop() ends
+  local variables exist ONLY in a function and are erase at the end of the function
+*/
+
+/*
+  Often, we want to see debug mesages about code
+  equivalent of print statements is Serial.println()
+
+  To use Serial.print, these are the steps
+  1) in setup(), add
+    Serial.begin(9600);
+  2) to display a message from Photon in Workbench, use the function Serial.println()
+  3) Open the SERIAL MONITOR from command palette
+*/
+
+const int PIN_LED = A2;
+const int PIN_POT = A0;
+
+int brightness = 0; //global var
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_POT, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //lets make the LED on for 2 sec, and off for 4 sec
+  // digitalWrite(PIN_LED, HIGH);  //send 3.3v (HIGH) to PIN_LED
+  // delay(2000);    //2000 millsec = 2 sec
+  // digitalWrite(PIN_LED, LOW); // LOW means 0v or ground
+  // delay(4000);
+   
+   //experiment with analog write
+   // values from 0 - 255 (0 is off or 0V, and 255 is on or HIGH 3.3v)
+  //  analogWrite(PIN_LED, 128); //half bright
+  //  delay(2000);
+  //  analogWrite(PIN_LED, 255); //full voltage
+  //  delay(2000);
+  //  analogWrite(PIN_LED, 0);
+  //  delay(2000);
+
+  //how can we get the brightness to go from 0 to 255 over time and then reset back to 0
+  // one approach is a for loop or a while loop
+  //      however, in this class we rarely use for loops
+          // and we NEVER use while loops
+
+    brightness = brightness + 10;
+    if (brightness >= 255) {
+      brightness = 0;
+    }
+    delay(500);
+    analogWrite(PIN_LED, brightness);
+    Serial.println("The brightness is " + String(brightness));
+    // we can not concatenate (use +) a string and int
+
+}
